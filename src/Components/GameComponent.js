f6c9b2f6be44403cf67af9db3089f39e7bec4dcb
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-09-24 15:47:03 +0500

    winner part 1

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -39,4 +39,24 @@ export class Game extends Component {
     };
 };
 
+function calculateWinner(squares) {
+    const lines= [
+        [0,1,2],
+        [3,4,5],
+        [5,7,8]
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if (squares[a] && square[a]===square[b] && square[b]===square[c]) {
+            return square[a];
+        }
+    }
+    return null;
+}
+
 export default Game;
